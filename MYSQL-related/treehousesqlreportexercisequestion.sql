d206ef0dd59b82a3b4f619d5e59e7142fb7c1756
kennethwu <kennethwu@leadstec.com>
2018-09-28 10:02:18 +0800

    Add teamtreehouse sql report exercise questions

---
+++ MYSQL-related/treehousesqlreportexercisequestion.sql
@@ -0,0 +1,10 @@
+-- Teamtreehouse SQL Report Exercise Questions
+
+
+
+-- total capacity of the school
+-- largest capacity of the school
+-- which subjects taught in romm 19
+-- which teachers teach students in 8th grade
+-- which teachers teach 7th grade science
+-- which teachers teach elective(subject with no grade)
