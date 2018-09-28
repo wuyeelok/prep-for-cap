f2d6970dd73fe3942a1f837712315decc5777729
kennethwu <kennethwu@leadstec.com>
2018-09-28 14:26:47 +0800

    Add more teamtreehouse exercise questions

--- MYSQL-related/treehousesqlreportexercisequestion.sql
+++ MYSQL-related/treehousesqlreportexercisequestion.sql
@@ -15,3 +15,11 @@
 -- generate a list of students with last name from A to M
 -- How many students are in each grade? And how many 6th graders do you think they'll have next year?
 -- Do they have room for that many 6th graders (226)?
+
+
+-- which teachers teach a class during all 7 period
+-- Do any teachers teach multiple subjects? if so, who an which subjects? -- what class does Janis Ambrose teach during each period be sure to inluce all 7 periods
+-- Which subject is the least popular, and how many students are taking it
+-- Which students have 5th period science and 7th perod art
+-- Which elective subject teacher teaches the most students
+-- Which teadcher don't have class during first period
