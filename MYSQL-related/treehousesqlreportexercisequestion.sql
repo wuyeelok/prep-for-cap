8a9452c20704d8f62a28a44301c7c74616c86934
kennethwu <kennethwu@leadstec.com>
2018-09-28 11:17:05 +0800

    Add more teamtreehouse exercise questions

--- MYSQL-related/treehousesqlreportexercisequestion.sql
+++ MYSQL-related/treehousesqlreportexercisequestion.sql
@@ -8,3 +8,10 @@
 -- which teachers teach students in 8th grade
 -- which teachers teach 7th grade science
 -- which teachers teach elective(subject with no grade)
+
+
+-- generate schdule for Rex Rios
+-- How many students have PE lesson during first period
+-- generate a list of students with last name from A to M
+-- How many students are in each grade? And how many 6th graders do you think they'll have next year?
+-- Do they have room for that many 6th graders (226)?
